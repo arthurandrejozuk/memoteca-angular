c415554d2c9c0fe7a79199848e1a892ea50b4673
Arthur Avila <arthurpro295@gmail.com>
2025-01-27 09:19:55 -0300

    Iniciando projeto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { PrincipalComponent } from './componentes/principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
